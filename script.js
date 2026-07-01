f261a74404d8384a71d07d85d991720e3157e49c
Pampu Rares <rarespampu@gmail.com>
2026-07-01 16:02:51 +0300

    improved functionality but still 1v1 problems

--- script.js
+++ script.js
@@ -1,52 +1,35 @@
 const minigameArea = document.getElementById("minigame-container")
 const infoSpan = document.getElementById("info-span")
+const modeSelector = document.getElementById("mode-selecter-container")
 
 const singlePlayerBtn = document.getElementById("single-player")
 const twoPlayerBtn = document.getElementById("2-player")
 
 const screenOverlay = document.getElementById("screen-overlay")
 
-const playerSpan = document.getElementById("player")
+const playerTurn = document.getElementById("player")
 
-let gameStarted = false, doNotPress = false
+const bestAttemptSpan = document.getElementById("best-attempt")
+const currentAttemptSpan = document.getElementById("current-attempt")
+
+const winnerDialog = document.getElementById("winner-dialog")
+
+let roundStarted = false, doNotPress = false
+let waitingInterval
 let startTime, endTime
-let player = 1, playerOneWins = 0, playerOneInterval, playerTwoWins = 0, playerTwoInterval, playsCount = 0;
+let player = 1, playerOneWins = 0, playerOneInterval, playerTwoWins = 0, playerTwoInterval, playsCount = 0
 
 function singlePlayerMode() {
-    if(!gameStarted)
-    {
-        infoSpan.innerText = 'Click when the color turns green'
+    if(!roundStarted) {
+        infoSpan.innerText = 'Click when the color turns blue'
         minigameArea.style.backgroundColor = 'red'
-        gameStarted = true
+        minigameArea.style.color = 'white'
+        roundStarted = true
         doNotPress = true
         const timeToWait = (Math.random() * 2000) + 1
-        setTimeout(() => {
-            minigameArea.style.backgroundColor = 'greenyellow'
-            infoSpan.innerText = 'Click'
-            doNotPress = false
-            startTime = performance.now()
-        }, timeToWait)
-    }
-    else if(doNotPress) infoSpan.innerText = 'You pressed to soon'
-    else {
-        endTime = performance.now()
-        timeInterval = (endTime - startTime).toPrecision(3) / 1000
-        infoSpan.innerText = 'You made it: ' + timeInterval + ' seconds. Click again when ready'
-        gameStarted = false
-
-    }
-}
-
-function twoPlayerMode() {
-    playsCount++
-    if(!gameStarted) {
-        infoSpan.innerText = 'Click when the color turns green'
-        minigameArea.style.backgroundColor = 'red'
-        gameStarted = true
-        doNotPress = true
-        const timeToWait = (Math.random() * 10000) + 1
-        setTimeout(() => {
-            minigameArea.style.backgroundColor = 'greenyellow'
+        waitingInterval = setTimeout(() => {
+            minigameArea.style.backgroundColor = 'var(--color-1)'
+            minigameArea.style.color = 'black'
             infoSpan.innerText = 'Click'
             doNotPress = false
             startTime = performance.now()
@@ -54,33 +37,98 @@ function twoPlayerMode() {
     }
     else if(doNotPress) {
         infoSpan.innerText = 'You pressed too soon'
-        if(player == 2) playerOneWins++
-        else playerTwoWins++
+        setTimeout(() => {
+            minigameArea.style.backgroundColor = 'var(--color-1)'
+            minigameArea.style.color = 'black'
+            infoSpan.innerText = 'Click'
+            doNotPress = false
+        }, 2000)
+        clearTimeout(waitingInterval)
     }
     else {
         endTime = performance.now()
         timeInterval = (endTime - startTime).toPrecision(3) / 1000
         infoSpan.innerText = timeInterval + ' seconds. Click again when ready'
-        gameStarted = false
-        if(player == 1) playerOneInterval += timeInterval
-        else playerTwoInterval += timeInterval
+        currentAttemptSpan.innerText = timeInterval
+        if(bestAttemptSpan.innerText === '' || Number(bestAttemptSpan.innerText) > timeInterval)
+            bestAttemptSpan.innerText = timeInterval
+        roundStarted = false
     }
-    if(playsCount == 3) {
-        player == 2;
-        gameStarted = false
-        doNotPress = false
-        player.innerText = 2;
+}
+
+function closePopup() {
+    screenOverlay.style.opacity = '1'
+    screenOverlay.style.pointerEvents = 'none'
+    playerTurn.innerText = 'Player 1'
+    setTimeout(() => {
+        winnerDialog.style.display = 'none'
+    }, 301)
+}
+
+function showWinner() {
+    winnerDialog.style.display = 'flex'
+    winnerDialog.innerHTML = `
+        <button id="close" onClick="closePopup()">X</button>
+        <h2>${playerOneInterval == playerTwoInterval || !playerOneInterval && !playerTwoInterval ? 'No one' : playerOneInterval < playerTwoInterval || !playerTwoInterval ? 'Player 1' : 'Player 2'} wins!</h2>
+        <div id="stats">
+            <p>Player 1</p>
+            <p style="${playerOneInterval <= playerTwoInterval || !playerTwoInterval ? 'color: white;' : ''}">${playerOneInterval}</p>
+            <p>Player 2</p>
+            <p style="${playerOneInterval >= playerTwoInterval || !playerOneInterval ? 'color: white;' : ''}">${playerTwoInterval}</p>
+        </div>
+    `
+    screenOverlay.style.opacity = '1'
+    screenOverlay.style.pointerEvents = 'auto'
+
+    playerTurn.innerText = ''
+
+}
+
+function twoPlayerMode() {
+    if(playsCount < 3) playerTurn.innerText = `Player 1`
+    else playerTurn.innerText = `Player 2`
+    if(!roundStarted) {
+        playsCount++
+        infoSpan.innerText = 'Click when the color turns blue'
+        minigameArea.style.backgroundColor = 'red'
+        minigameArea.style.color = 'white'
+        roundStarted = true
+        doNotPress = true
+        const timeToWait = (Math.random() * 2000) + 1
+        waitingInterval = setTimeout(() => {
+            minigameArea.style.backgroundColor = 'var(--color-1)'
+            minigameArea.style.color = 'black'
+            infoSpan.innerText = 'Click'
+            doNotPress = false
+            startTime = performance.now()
+        }, timeToWait)
     }
-    if(playsCount == 6) {
-        playerOneInterval /= 3;
-        playerTwoInterval /= 3;
-        if(playerOneInterval > playerTwoInterval) playerTwoWins++;
-        else playerOneWins++;
-        playsCount = 0;
-        player = 1;
-        console.log('Score')
-        console.log('Player 1:' + playerOneWins)
-        console.log('Player 2: ' + playerTwoWins)
+    else if(doNotPress) {
+        infoSpan.innerText = 'You pressed too soon'
+        setTimeout(() => {
+            minigameArea.style.backgroundColor = 'var(--color-1)'
+            minigameArea.style.color = 'black'
+            infoSpan.innerText = 'Click'
+            doNotPress = false
+            roundStarted = false
+        }, 2000)
+        clearTimeout(waitingInterval)
+        if(playsCount == 3) playerTurn.innerText = "It is now player 2's turn"
+        else if(playsCount == 6) showWinner()
+    }
+    else {
+        endTime = performance.now()
+        roundStarted = false
+        timeInterval = (endTime - startTime).toPrecision(3) / 1000
+        infoSpan.innerText = timeInterval + ' seconds. Click again when ready'
+        currentAttemptSpan.innerText = timeInterval
+        if(bestAttemptSpan.innerText === '' || Number(bestAttemptSpan.innerText) > timeInterval) {
+            bestAttemptSpan.innerText = timeInterval
+            if(playsCount <=3) playerOneInterval = timeInterval
+            else playerTwoInterval = timeInterval
+        }
+        if(playsCount == 3) playerTurn.innerText = "It is now player 2's turn"
+        else if(playsCount == 6) showWinner()
     }
 }
 
@@ -92,11 +140,18 @@ minigameArea.addEventListener("click", () => {
 singlePlayerBtn.addEventListener("click", () => {
     screenOverlay.style.opacity = '0'
     screenOverlay.style.pointerEvents = 'none'
+    setTimeout(() => {
+        modeSelector.style.display = 'none'
+    }, 301)
     singlePlayer = true
 })
 
 twoPlayerBtn.addEventListener("click", () => {
     screenOverlay.style.opacity = '0'
     screenOverlay.style.pointerEvents = 'none'
+    setTimeout(() => {
+        modeSelector.style.display = 'none'
+    }, 301)
     singlePlayer = false
+    playerTurn.innerText = `Player 1`
 })
