ab0115c0df9495d8f721538aa0a1efa8ef4ef076
Pampu Rares <rarespampu@gmail.com>
2026-07-09 16:21:10 +0300

    mostly works, target jumps out sometimes

--- scripts/targetTesterScript.js
+++ scripts/targetTesterScript.js
@@ -1,3 +1,4 @@
+const appContainer = document.getElementById("app-container")
 const minigameArea = document.getElementById("minigame-container")
 const infoSpan = document.getElementById("info-span")
 
@@ -15,17 +16,54 @@ const targetsHitSpan = document.getElementById("targets-hit")
 const winnerDialog = document.getElementById("winner-dialog")
 
 const playersScores = document.getElementById("players-scores");
-const playerOneScore = document.getElementById("player-1-score");
-const playerTwoScore = document.getElementById("player-2-score");
+const playerOneTime = document.getElementById("player-1-time");
+const playerOneHits = document.getElementById("player-1-hits");
+const playerTwoTime = document.getElementById("player-2-time");
+const playerTwoHits = document.getElementById("player-2-hits");
 
 const playBtn = document.getElementById("play-btn")
 const target = document.getElementById("target")
 
+const difficultySelector = document.getElementById("difficulty-selector")
+
 let roundStarted = false, isResetting = false, singlePlayer = true, noLatency = true
 let playsCount = 0
-let startTime, endTime
-let player = 1, playerOneWins = 0, playerOneInterval, playerTwoWins = 0, playerTwoInterval
+let startTime, endTime, targetDifficulty = 'easy-difficulty'
+let player = 1, playerOneWins = 0, playerTwoWins = 0
 
+
+function resetGame() {
+    appContainer.classList.remove('blurred')
+    playsCount = 0
+    roundStarted = false
+    target.style.display = 'none'
+    playBtn.style.display = 'inline-block'
+    minigameArea.style.backgroundColor = 'var(--color-1)'
+    screenOverlay.style.opacity = '0'
+    screenOverlay.style.pointerEvents = 'none'
+    if(!singlePlayer) {
+        if(player === 2)
+            playBtn.innerText = 'Player 2: Play'
+        else {
+            playBtn.innerText = 'Player 1: Play'
+            playerOneTime.innerText = 'TBD'
+            playerOneHits.innerText = '0/20'
+            playerTwoTime.innerText = 'TBD'
+            playerTwoHits.innerText = '0/20'
+        }
+    } else playBtn.innerText = 'Play'
+    targetsMissedSpan.innerText = '0'
+    targetsHitSpan.innerText = '0/10'
+    const heightMultiplier = Math.random() * (minigameArea.offsetHeight - target.offsetHeight)
+    const widthMultiplier = Math.random() * (minigameArea.offsetWidth - target.offsetWidth)
+    target.style.top = String(heightMultiplier) + 'px'
+    target.style.left = String(widthMultiplier) + 'px'
+    setTimeout(() => {
+        modeSelector.style.display = 'none'
+        winnerDialog.style.display = 'none'
+        screenOverlay.style.display = 'none'
+    }, 301)
+}
 
 function endRound() {
     endTime = performance.now()
@@ -34,21 +72,63 @@ function endRound() {
     target.style.display = 'none'
     winnerDialog.style.display = 'flex'
     winnerDialog.innerHTML = `
-        <button id="close" onClick="closePopup()">X</button>
-        <h2 style="color: black;">See your stats</h2>
+        <button id="close" onClick="${player === 2 ? 'showWinner()' : 'resetGame()'}">X</button>
+        <h2 style="color: black;">${singlePlayer ? 'See your' : 'Player ' + player} stats</h2>
         <div id="stats">
             <p style="color: black;">Total time:</p>
             <p style="">${((endTime - startTime) / 1000).toFixed(3)}s</p>
             <p style="color: black;">Targets Missed:</p>
-            <p style="">${String(targetsMissedSpan.innerText)}</p>
+            <p style="${Number(targetsMissedSpan.innerText) ? 'color: red;' : ''}">${targetsMissedSpan.innerText}</p>
         </div>
     `
+    if(!singlePlayer) {
+        if(player === 1) {
+            player = 2
+            playerOneTime.innerText = String(((endTime - startTime) / 1000).toFixed(3)) + 's'
+        } else {
+            player = 1
+            playerTwoTime.innerText = String(((endTime - startTime) / 1000).toFixed(3)) + 's'
+        } 
+    }
+    screenOverlay.style.display = 'flex'
     screenOverlay.style.opacity = '1'
     screenOverlay.style.pointerEvents = 'auto'
     playBtn.style.display = 'inline-block' // it may be block
+    appContainer.classList.add('blurred')
 }
 
-function singlePlayerMode(missed = false) {
+function showWinner() {
+    let playerOneInterval = Number(playerOneTime.innerText.slice(0, playerOneTime.innerText.length - 1))
+    let playerOneMisses = 20 - playerOneHits.innerText.split('/')[0]
+    let playerTwoInterval = Number(playerTwoTime.innerText.slice(0, playerTwoTime.innerText.length - 1))
+    let playerTwoMisses = 20 - playerTwoHits.innerText.split('/')[0]
+    playerOneInterval += playerOneMisses * 2.0
+    playerTwoInterval += playerTwoMisses * 2.0
+    let winner
+    if(playerOneInterval < playerTwoInterval) winner = 1
+    else if(playerTwoInterval < playerOneInterval) winner =  2
+    winnerDialog.innerHTML = `
+        <button id="close" onClick="resetGame()">X</button>
+        <h2 style="color: black;">${winner === 1 ? 'Player 1' : winner === 2 ? 'Player 2' :'No one'} wins!</h2>
+        <div id="extended-stats">
+            <p style="${winner === 2 ? 'color: black;' : ''}">Player 1</p>
+            <p style="${winner === 2 ? 'color: black;' : ''}">${playerOneInterval}</p>
+            <p style="${winner === 2 ? 'color: black;' : ''}">${20 - playerOneMisses}/20</p>
+            <p style="${winner === 1 ? 'color: black;' : ''}">Player 2</p>
+            <p style="${winner === 1 ? 'color: black;' : ''}">${playerTwoInterval}</p>
+            <p style="${winner === 1 ? 'color: black;' : ''}">${20 - playerTwoMisses}/20</p>
+        </div>
+    `
+    player = 1
+}
+
+function twoPlayerMode() {
+    if(player == 1) {
+        playerOneHits.innerText = targetsHitSpan.innerText
+    } else playerTwoHits.innerText = targetsHitSpan.innerText
+}
+
+function playGame(missed = false) {
     if(missed) {
         targetsMissedSpan.innerText = String(Number(targetsMissedSpan.innerText) + 1)
         minigameArea.style.backgroundColor = 'Red'
@@ -58,9 +138,9 @@ function singlePlayerMode(missed = false) {
     }
     else {
         targetsHitSpan.innerText = String(Number(targetsHitSpan.innerText.split('/')[0]) + 1) + '/20'
+        if(!singlePlayer) twoPlayerMode()
     }
     playsCount++;
-    console.log(playsCount)
     if(playsCount == 20) endRound()
     else if(noLatency) {
         const heightMultiplier = Math.random() * (minigameArea.offsetHeight - target.offsetHeight)
@@ -72,67 +152,20 @@ function singlePlayerMode(missed = false) {
     }
 }
 
-function closePopup() {
-    screenOverlay.style.opacity = '0'
-    screenOverlay.style.pointerEvents = 'none'
-    if(!singlePlayer) {
-        playerTurn.innerText = 'Player 1'
-        playerOneInterval = undefined
-        playerTwoInterval = undefined
-        playerOneScore.innerText = ''
-        playerTwoScore.innerText = ''
-    }
-    targetsMissedSpan.innerText = '0'
-    targetsHitSpan.innerText = '0/10'
-    setTimeout(() => {
-        winnerDialog.style.display = 'none'
-        screenOverlay.style.display = 'none'
-    }, 301)
-}
-
-function showWinner() {
-    winnerDialog.style.display = 'flex'
-    winnerDialog.innerHTML = `
-        <button id="close" onClick="closePopup()">X</button>
-        <h2 style="color: white;">${playerOneInterval == playerTwoInterval || !playerOneInterval && !playerTwoInterval ? 'No one' : playerOneInterval < playerTwoInterval || !playerTwoInterval ? 'Player 1' : 'Player 2'} wins!</h2>
-        <div id="stats">
-            <p style="${playerOneInterval <= playerTwoInterval || !playerTwoInterval ? 'color: white;' : ''}">Player 1</p>
-            <p style="${playerOneInterval <= playerTwoInterval || !playerTwoInterval ? 'color: white;' : ''}">${playerOneInterval}</p>
-            <p style="${playerOneInterval >= playerTwoInterval || !playerOneInterval ? 'color: white;' : ''}">Player 2</p>
-            <p style="${playerOneInterval >= playerTwoInterval || !playerOneInterval ? 'color: white;' : ''}">${playerTwoInterval}</p>
-        </div>
-    `
-    screenOverlay.style.opacity = '1'
-    screenOverlay.style.pointerEvents = 'auto'
-
-    playerTurn.innerText = ''
-    bestAttemptSpan.innerText = ''
-    currentAttemptSpan.innerText = ''
-}
-
-function twoPlayerMode() {
-}
-
 function hideMainDialog() {
+    appContainer.classList.remove('blurred')
     screenOverlay.style.opacity = '0'
     screenOverlay.style.pointerEvents = 'none'
     setTimeout(() => {
         modeSelector.style.display = 'none'
+        //screenOverlay.style.display = 'none'
     }, 301)
 }
 
-function resetGame() {
-    roundStarted = false
-    doNotPress = false
-    minigameArea.style.backgroundColor = 'var(--color-1)'
-    minigameArea.style.color = 'black'
-    infoSpan.innerText = 'Click'
-}
 
 minigameArea.addEventListener("click", () => {
     if(roundStarted) {
-        if(!singlePlayer) twoPlayerMode(true)
-            else singlePlayerMode(true)
+        playGame(true)
     }
 })
 
@@ -140,17 +173,13 @@ singlePlayerBtn.addEventListener("click", () => {
     hideMainDialog()
     playersScores.style.display = 'none'
     singlePlayer = true
-    //resetGame()
+    resetGame()
 })
 
 twoPlayerBtn.addEventListener("click", () => {
-    playersScores.style.display = 'grid'
     hideMainDialog()
+    playersScores.style.display = 'grid'
     singlePlayer = false
-    bestAttemptSpan.innerText = ''
-    currentAttemptSpan.innerText = ''
-    playerTurn.innerText = `Player 1`
-    playsCount = 0
     resetGame()
 })
 
@@ -164,11 +193,11 @@ playBtn.addEventListener("click", () => {
 
 target.addEventListener("click", () => {
     event.stopPropagation()
-    if(singlePlayer) singlePlayerMode()
-        else twoPlayerMode()
+    playGame()
 })
 
 selectModeBtn.addEventListener("click", () => {
+    appContainer.classList.add('blurred')
     modeSelector.style.display = 'flex'
     screenOverlay.style.display = 'flex'
 
@@ -178,4 +207,13 @@ selectModeBtn.addEventListener("click", () => {
 
 changeGamemodeBtn.addEventListener("click", () => {
     window.location = './index.html'
+})
+
+difficultySelector.addEventListener("change", () => {
+    target.classList.toggle(targetDifficulty)
+    if(difficultySelector.value === 'easy') targetDifficulty = 'easy-difficulty'
+    else if(difficultySelector.value === 'medium') targetDifficulty = 'medium-difficulty'
+    else targetDifficulty = 'hard-difficulty'
+    target.classList.toggle(targetDifficulty)
+    resetGame()
 })
